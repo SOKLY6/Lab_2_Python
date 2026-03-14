852935d9dd52c9891b3ee925196954b5f52bc761
Sokly6 <clavander07@gmail.com>
2026-03-14 10:55:40 +0300

    Beginning creating TaskQueue

--- src/usecases/task_queue.py
+++ src/usecases/task_queue.py
@@ -1,4 +1,4 @@
-from queue import Queue
+from asyncio import PriorityQueue
 from typing import Iterable
 
 from src.domain.descriptors import CorrectTaskPriority, CorrectTaskStatus
@@ -9,60 +9,33 @@ from src.domain.task import Task
 class TaskQueue:
     """Класс для работы с задачами"""
 
-    max_priority = CorrectTaskPriority()
-    min_priority = CorrectTaskPriority()
+    priority = CorrectTaskPriority()
     status = CorrectTaskStatus()
 
-    def __init__(self, max_priority: int = 5) -> None:
-        self.max_priority = max_priority
-        self.priority_task_dict: dict[int, Queue] = {
-            priority: Queue() for priority in range(self.max_priority, 0, -1)
-        }
-
-    def __iter__(self) -> Iterable[Task]:
-        for priority in range(self.max_priority, 0, -1):
-            for task in self.priority_task_dict[priority].queue:
-                yield task
+    def __init__(self, priority_queue: PriorityQueue, max_priority: int = 5) -> None:
+        self.queue = priority_queue
 
     def __len__(self) -> int:
-        return sum(q.qsize() for q in self.priority_task_dict.values())
+        return self.queue.qsize()
+
+    def __iter__(self) -> Iterable[Task]:
+        for _ in range(len(self)):
+            yield self.queue.get()
 
     def __bool__(self) -> bool:
         return True if len(self) > 0 else False
-
-    def __contains__(self, task_in: Task) -> bool:
-        """Проверяет, есть ли задача в очереди"""
-        for task_list_number in range(self.max_priority, 0, -1):
-            task: Task
-            for task in self.priority_task_dict[task_list_number].queue:
-                if task_in.id == task.id:
-                    return True
-        return False
-
-    def push(self, task: Task) -> None:
-        if task.priority >= self.max_priority:
-            raise IncorrectTaskPriority(
-                "Приоритет задачи должен быть меньше максимально возможного"
-            )
-
-        self.priority_task_dict[task.priority].put(task)
-
-    def pop(self) -> Task:
-        for q in self.priority_task_dict.values():
-            if not q.empty():
-                return q.get()
-        raise IndexError("Очередь пустая")
+    
+    def no_filter(self) -> Iterable[Task]:
+        return iter(self)
 
     def filter_status(self, status: str) -> Iterable[Task]:
         self.status = status
-        for priority in range(self.max_priority, 0, -1):
-            task: Task
-            for task in self.priority_task_dict[priority].queue:
-                if task.status == self.status:
-                    yield task
+        task: Task
+        for task in iter(self):
+            if task.status == status:
+                yield task
 
-    def filter_priority(self, min_priority: int) -> Iterable[Task]:
-        self.min_priority = min_priority
+    def filter_priority(self, max_priority: int) -> Iterable[Task]:
         if self.min_priority >= self.max_priority:
             raise IncorrectTaskPriority(
                 "Минимальный приоритет должен быть "
